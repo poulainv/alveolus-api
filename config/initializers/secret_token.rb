f52ccf9eb5cb0a66bf4290af082bfb8028af01cf
Vincent Poulain <vincent.poulain2@gmail.com>
2012-10-18 20:39:26 +0200

    First commit
    skeleton architecture

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnjoyTheWeb::Application.config.secret_token = 'dce1618613c1da0706ace3cf1222b9e1a41be11119600a96f3e0e98a810cc3cb179c51779d04f4385acca4e0473e822c6ab852f270407403957ef2067c162ad3'
