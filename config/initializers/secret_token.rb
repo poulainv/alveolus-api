3ee4ea1d76b442dc99188c422acea87452531fac
Vincent Poulain <vincent.poulain2@gmail.com>
2012-10-19 14:29:58 +0200

    kk
    k

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnjoyTheWeb::Application.config.secret_token = '91ea1c59fd369f56faba63a2ef045097f8d944e2d2d974f3094f90c919f90d241764109b0ade612791bf68b1bbe92ae91dd4aafad76a607773952f972e8c1e3e'
