8b0a792c6685d5c413ad064a5c6798186eb66c2b
Vincent Poulain <vincent.poulain2@gmail.com>
2012-10-19 14:25:18 +0200

    delete

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-EnjoyTheWeb::Application.config.secret_token = 'fe4cf3b7e433769c37ca60004541440f38f8928877b7d0065b5fe01ee1da7cd8dd7efc43404fb4ae12e9c75acd8a8077449de8898e18c025171bfcf602578b15'
