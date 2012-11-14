1927f1471af4229d0f865d148c052565f056e30f
vpoulain <vincent.poulain2@gmail>
2012-11-14 03:35:52 +0100

    remove initializer paperclip

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,4 @@
 # To change this template, choose Tools | Templates
 # and open the template in the editor.
 
-Paperclip.options[:image_magick_path] = '/usr/local/bin/'
+#Paperclip.options[:image_magick_path] = '/usr/local/bin/'
