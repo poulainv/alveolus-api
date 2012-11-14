82b36800ecd06b59ee6718caedfd8481b9edbbbb
vpoulain <vincent.poulain2@gmail>
2012-11-14 23:18:05 +0100

    add log for store image on cloud amazon s3

---
+++ app/assets/javascripts/caroussel/caroussel.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+// Si on veut faire apparaitre le caption seulement
+//  au passage de la souris sur l'image'
+//    $(".carousel-caption").hide();
+//    $(".item").mouseover(function () {
+//        $(".carousel-caption").show();
+//    });
+//    $(".item").mouseout(function () {
+//        $(".carousel-caption").hide();
+//    });
+
+});
