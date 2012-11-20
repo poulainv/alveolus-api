9a288ad8ba67d4a1dd9938ea96b4d98ef5a37984
vpoulain <vincent.poulain2@gmail>
2012-11-20 22:28:23 +0100

    css carousel change

--- app/assets/javascripts/caroussel/caroussel.js
+++ app/assets/javascripts/caroussel/caroussel.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
 
 // Si on veut faire apparaitre le caption seulement
 //  au passage de la souris sur l'image'
-//    $(".carousel-caption").hide();
-//    $(".item").mouseover(function () {
-//        $(".carousel-caption").show();
-//    });
-//    $(".item").mouseout(function () {
-//        $(".carousel-caption").hide();
-//    });
+        $(".carousel-caption").hide();
+        $(".item").mouseover(function () {
+            $(".carousel-caption").show(0);
+        });
+        $(".item").mouseout(function () {
+            $(".carousel-caption").hide(0);
+        });
 
 });
