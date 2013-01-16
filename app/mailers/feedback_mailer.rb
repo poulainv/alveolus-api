f3d21d74499c7e90fac9ff1a74f43f251a4a0a71
vpoulain <vincent.poulain2@gmail>
2013-01-16 16:00:58 +0100

    tag mobilté

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,5 +1,5 @@
 class FeedbackMailer < ActionMailer::Base
-  default from: "enjoytheweb2@gmail.com"
+  default from: "EnjoyTheWeb2@gmail.com"
 
   def feedback(feedback)
     recipients  = 'vincent.poulain2@gmail.com'
--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => "enjoytheweb2@gmail.com",
+   :user_name            => "EnjoyTheWeb2@gmail.com",
    :domain            => "gmail.com",
    :password             => "quenelle",
    :authentication       => "plain",
