38e4f9e402876077b2e0c06eedd21be108725367
vpoulain <vincent.poulain2@gmail.com>
2013-04-20 11:38:16 +0200

    Remove basic_auth add fix cross domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  http_basic_authenticate_with :name => "admin", :password => "quenelle"
+  #http_basic_authenticate_with :name => "admin", :password => "quenelle"
+
+   # do not use CSRF for CORS options
+  skip_before_filter :verify_authenticity_token, :only => [:options]
+
+  before_filter :cors_set_access_control_headers
+
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = 'http://localhost:8000'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*, X-Requested-With, X-Prototype-Version, X-CSRF-Token, Content-Type'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def options 
+    render :text => '', :content_type => 'text/plain'
+  end
+  
 end
