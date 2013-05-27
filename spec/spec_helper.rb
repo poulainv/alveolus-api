18b1211e44283e86cc14d9343f87306e6d26a5f6
Bertrand <bertrand.louis.bon@wanadoo.fr>
2013-05-27 20:57:52 +0200

    Add login spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,25 @@ require 'rspec/rails'
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
+
+module ControllerMacros
+  def login_admin
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:admin]
+      sign_in FactoryGirl.create(:admin_user) # Using factory girl as an example
+    end
+  end
+
+  def login_user
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      user = FactoryGirl.create(:user)
+      #user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the confirmable module
+      sign_in user
+    end
+  end
+end
+
 RSpec.configure do |config|
   # == Mock Framework
   #
@@ -17,6 +36,8 @@ RSpec.configure do |config|
   # config.mock_with :rr
   config.mock_with :rspec
   config.include JsonSpec::Helpers
+  config.include Devise::TestHelpers, :type => :controller
+  config.extend ControllerMacros, :type => :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -25,3 +46,4 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 end
+
