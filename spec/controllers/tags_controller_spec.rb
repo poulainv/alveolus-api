55d2317d58d14e1648c64e5165c78c23dd66049f
Bertrand <bertrand.louis.bon@wanadoo.fr>
2013-05-28 19:46:05 +0200

    Add tags controller specs for index & show

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -1,7 +1,71 @@
 require 'spec_helper'
 
 describe TagsController do
+  render_views
 
+  before(:each) do
+    (0..2).each do
+      FactoryGirl.create(:tag)
+    end
+  end
+
+  describe "GET index" do
+    it "should return a success http" do
+      get :index
+      response.should be_success
+    end
+
+    it "should return a valid json" do
+      get :index
+      expect { parse_json(response.body) }.should_not raise_error(MultiJson::DecodeError)
+    end
+
+    it "should return 3 tags" do
+      get :index
+      response.body.should have_json_size(Tag.count)
+    end
+
+    it "should have integer id" do
+      get :index
+      response.body.should have_json_path("0/id")
+      response.body.should have_json_type(Integer).at_path("0/id")
+      parse_json(response.body, "0/id").should == Tag.first.id
+    end
+
+    it "should have string name" do
+      get :index
+      response.body.should have_json_path("0/name")
+      response.body.should have_json_type(String).at_path("0/name")
+      parse_json(response.body, "0/name").should == Tag.first.name
+    end
+  end
+
+  describe "GET show" do
+    it "should return a success http" do
+      get :show, id: Tag.first
+      puts response.body
+      response.should be_success
+    end
+
+    it "should return a valid json" do
+      get :show, id: Tag.first
+      expect { parse_json(response.body) }.should_not raise_error(MultiJson::DecodeError)
+    end
+
+    it "should have integer id" do
+      get :show, id: Tag.first
+      response.body.should have_json_path("id")
+      response.body.should have_json_type(Integer).at_path("id")
+      parse_json(response.body, "id").should == Tag.first.id
+    end
+
+    it "should have string name" do
+      get :show, id: Tag.first
+      response.body.should have_json_path("name")
+      response.body.should have_json_type(String).at_path("name")
+      parse_json(response.body, "name").should == Tag.first.name
+    end
+  end
 
   ## Test NEW method
   # describe "GET 'new'" do
