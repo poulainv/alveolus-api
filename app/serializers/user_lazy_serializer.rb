cd0f0338096b0e20c1805730642dc17d36a1eee9
vpoulain <vincent.poulain2@gmail.com>
2013-06-04 00:07:38 +0200

    Add user to unvalidated web app json

--- app/serializers/user_lazy_serializer.rb
+++ app/serializers/user_lazy_serializer.rb
@@ -1,5 +1,5 @@
 class UserLazySerializer < ActiveModel::Serializer
-  attributes :id,:pseudo, :created_at, :sign_in_count, :image_url
+  attributes :id,:pseudo, :image_url
 
 	def image_url
 		object.image_url(:small)
--- app/serializers/webapp_vote_serializer.rb
+++ app/serializers/webapp_vote_serializer.rb
@@ -1,12 +1,13 @@
 class WebappVoteSerializer < ActiveModel::Serializer
-  attributes :id,:caption,:title,:description,:average_rate,:image_url,:count_positive,:count_negative,:my_vote, :facebook_id,:twitter_id, :gplus_id,:vimeo_id,:user_id, :nb_click_url,:nb_click_detail,:url, :validate, :category_id
-  has_many :comments, embed: :ids, :key => :comments
-  has_many :tags
-  has_one :category, :serializer => CategoryLazySerializer
+	attributes :id,:caption,:title,:description,:image_url,:count_positive,:count_negative,:my_vote, :facebook_id,:twitter_id, :gplus_id,:vimeo_id,:user_id, :nb_click_url,:nb_click_detail,:url, :category_id
+	# has_many :comments, embed: :ids, :key => :comments
+	has_many :tags
+	has_one :user, :serializer => UserLazySerializer
+	has_one :category, :serializer => CategoryLazySerializer
 
-  def image_url
-  	object.image_url(:medium)
-  end
+	def image_url
+		object.image_url(:medium)
+	end
 
 	def count_positive
 		object.count_positive
