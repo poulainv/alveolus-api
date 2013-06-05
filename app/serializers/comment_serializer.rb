cbcf701b5f7fc3f08daa362b8a5d0735d8ffcdae
vpoulain <vincent.poulain2@gmail.com>
2013-06-05 23:20:40 +0200

    Try to add info ton comment json

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,7 +1,18 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :body, :rating, :created_at
+  attributes :id, :body, :rating, :created_at, :webapp_id, :webapp_image_url, :webapp_title
   has_one :user, :serializer => UserLazySerializer
+ 
+def webapp_id
+	object.webapp.id
+end
 
+def webapp_image_url
+	webapp = object.webapp.photo(:small)
+end
+
+def webapp_title
+	object.webapp.title
+end
 
 
 
