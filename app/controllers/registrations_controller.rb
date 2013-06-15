6cb2085407d74b22dffd3f6c7f37f4772694b1f8
vpoulain <vincent.poulain2@gmail.com>
2013-06-15 17:05:39 +0200

    generate random pseudo

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,6 +10,7 @@ class RegistrationsController < Devise::RegistrationsController
      build_resource(params[:user])
     # resource.build
     if resource.save
+      resource.update_attribute(:pseudo,"alveoler"+resource.id.to_s)
       # sign_in(:user, resource)
       resource.ensure_authentication_token!
        # render :json=> {:success=>true, :auth_token=>resource.authentication_token, :email=>resource.email, :id => resource.id}
