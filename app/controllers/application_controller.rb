42712b987d1aff08eafa8a2d2135df6f4f436557
vpoulain <vincent.poulain2@gmail.com>
2013-06-18 14:08:17 +0200

    delete cross domain exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
 
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = 'http://localhost:8000'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
     headers['Access-Control-Allow-Headers'] = '*, X-Requested-With, X-Prototype-Version, X-CSRF-Token, Content-Type, X-AUTH-TOKEN'
     headers['Access-Control-Max-Age'] = "1728000"
   end
